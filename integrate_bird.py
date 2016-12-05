3c0fcb6698db039111526456a9a26dbf5adbc313
josueortc <josueortc@gmail.com>
2016-12-05 07:04:58 -0600

    Modified integrate_bird.py

--- integrate_bird.py
+++ integrate_bird.py
@@ -155,8 +155,7 @@ def produce_video(camera_position,type_bird, colition,final_frame,L, bird_positi
 	    rot_quat = direction.to_track_quat('-Z', 'Y')
 	    # assume we're using euler rotation
 	    obj_camera.rotation_euler = rot_quat.to_euler()
-	    obj_camera.keyframe_insert(data_path ='Rotation',group="LocRot")
-
+         
 	def get_finalposition(camera,L):
 		vec = mathutils.Vector((L*math.sin(math.pi*random.uniform(0,1),0.0,L*math.cos(math.pi*random.uniform(0,1)))))
 		inv = camera.matrix_world.copy()
@@ -189,33 +188,41 @@ def produce_video(camera_position,type_bird, colition,final_frame,L, bird_positi
 
 	# add the group instance to the scene
 	scene = bpy.context.scene
+	for ob in scene.objects:
+         if ob.type == 'MESH' and ob.name.startswith("Cube"):
+             ob.select = True
+         else: 
+             ob.select = False
+	bpy.ops.object.delete()
+         
 	scene.frame_end = final_frame
 	obj_camera = bpy.data.objects["Camera"]
 	for group in data_dst.groups:
-		ob = bpy.data.objects.new(group.name, None)
-		ob.dupli_group = group
-		ob.dupli_type = 'GROUP'
-		scene.objects.link(ob)
+          ob = bpy.data.objects.new(group.name, None)
+          ob.dupli_group = group
+          ob.dupli_type = 'GROUP'
+          scene.objects.link(ob)
 	    #look_at(ob, obj_camera.matrix_world.to_translation())
 
 		#obj_camera.select = True
-		#ob.select = True
-		ob.select = True
-		bpy.context.scene.frame_set(1)
+          #ob.select = True
+          ob.select = True
           if colition == 1:
-               look_at(obj_camera,ob.location)
-		if type_bird == 1:
-	    		ob.scale = (0.4,0.4,0.4)
-	    		ob.location = bird_position
-	    		ob.keyframe_insert(data_path ='location',group="LocRot")
+              look_at(obj_camera,ob.matrix_world.to_translation())
+          bpy.context.scene.frame_set(1)
+          
+          if type_bird == 1:
+              ob.scale = (0.4,0.4,0.4)
+              ob.location = bird_position
+              ob.keyframe_insert(data_path ='location',group="LocRot")
 
-		bpy.context.scene.frame_set(final_frame)
-		if colition == 1:
-	    		ob.location = tuple(obj_camera.location)
-		else:
-			ob.location = get_finalposition(obj_camera,L)
+          bpy.context.scene.frame_set(final_frame)
+          if colition == 1:
+              ob.location = obj_camera.matrix_world.to_translation()
+          else:
+              ob.location = get_finalposition(obj_camera,L)
 
-		ob.keyframe_insert(data_path ='location',group='LocRot')
+          ob.keyframe_insert(data_path ='location',group='LocRot')
 
 
 
@@ -234,11 +241,8 @@ def produce_video(camera_position,type_bird, colition,final_frame,L, bird_positi
 		distance = math.sqrt((lst[0][0] - lst[1][0])**2 + (lst[0][1] - lst[1][1])**2 + (lst[0][2] - lst[1][2])**2)
 		distances.append(distance)
 
-
 	#Save distance to the camera for further analysis
-	f = open('data_{}_{}_{}.pkl'.format(type_bird,object_velocity,colition,'wb')
-	pickle.dump(distances,f,2)
-	f.close()
+
 	object_velocity = float(distances[0])/float((float(final_frame)/float(FPS)))
 	bpy.data.scenes["Scene"].frame_end = final_frame
 	bpy.data.scenes["Scene"].render.fps = FPS
@@ -246,8 +250,12 @@ def produce_video(camera_position,type_bird, colition,final_frame,L, bird_positi
 	bpy.data.scenes["Scene"].render.filepath = '/home/josue/looming_stimuli_{}_{}_{}.avi'.format(type_bird,object_velocity,colition)
 	bpy.ops.render.render( animation=True )
 
+	name = 'data_{}_{}_{}.pkl'.format(type_bird,object_velocity,colition)
+	f = open(name,'wb')
+	pickle.dump(distances,f,protocol=pickle.HIGHEST_PROTOCOL)
+	f.close()
 
 '''
 To execute use: blender -b theevanmeeyaswalk1.blend -P integrate_bird.py 
 '''
-produce_video(camera_position = (4.15445,13.74561,-4.82148), bird_position = (14.15445,23.74561,4.82148),FPS = 24.0,type_bird=1, colition=1,final_frame=115,L=2)
+produce_video(camera_position = (4.15445,13.74561,-4.82148), bird_position = (4.15445,23.74561,4.82148),FPS = 24.0,type_bird=1, colition=1,final_frame=115,L=2)
