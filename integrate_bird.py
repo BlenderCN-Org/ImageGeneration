833160b7397bd3d862e57af20fd01df28c1db38f
josueortc <josueortc@gmail.com>
2016-12-04 22:50:22 -0600

    Add everything to integrate_bird.py

--- integrate_bird.py
+++ integrate_bird.py
@@ -1,11 +1,152 @@
-def produce_video(camera_position, bird_position = (14.15445,23.74561,4.82148),FPS = 24.0,type_bird, colition,final_frame,L):
+'''
+Create Bounding Box for the whole bird and then calculate the radius (Which is the L in the LGMD model)
+'''
+import bpy
+import bmesh
+from bpy.props import BoolProperty, FloatVectorProperty
+import mathutils
+from bpy_extras import object_utils
+
+def CreateBoundingBox(context):
+    minx, miny, minz = (999999.0,)*3
+    maxx, maxy, maxz = (-999999.0,)*3
+    for obj in context.selected_objects:
+        for v in obj.bound_box:
+            v_world = obj.matrix_world * mathutils.Vector((v[0],v[1],v[2]))
+            if v_world[0] < minx:
+                minx = v_world[0]
+            if v_world[0] > maxx:
+                maxx = v_world[0]
+            if v_world[1] < miny:
+                miny = v_world[1]
+            if v_world[1] > maxy:
+                maxy = v_world[1]
+		    if v_world[2] < minz:
+		        minz = v_world[2]
+		    if v_world[2] > maxz:
+			    maxz = v_world[2]
+
+    values = (((maxx-minx)/2.0) + ((maxz-minz)/2.0) + ((maxy-miny)/2.0))/6.0
+	return values
+
+import bpy
+from mathutils import Vector
+
+class Box:
+
+    dim_x = 1
+    dim_y = 1
+
+    def __init__(self, min_x, min_y, max_x, max_y, dim_x=dim_x, dim_y=dim_y):
+        self.min_x = min_x
+        self.min_y = min_y
+        self.max_x = max_x
+        self.max_y = max_y
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+
+    @property
+    def x(self):
+        return round(self.min_x * self.dim_x)
+
+    @property
+    def y(self):
+        return round(self.dim_y - self.max_y * self.dim_y)
+
+    @property
+    def width(self):
+        return round((self.max_x - self.min_x) * self.dim_x)
+
+    @property
+    def height(self):
+        return round((self.max_y - self.min_y) * self.dim_y)
+
+    def __str__(self):
+        return "<Box, x=%i, y=%i, width=%i, height=%i>" % \
+               (self.x, self.y, self.width, self.height)
+
+    def to_tuple(self):
+        if self.width == 0 or self.height == 0:
+            return (0, 0, 0, 0)
+        return (self.x, self.y, self.width, self.height)
+
+def camera_view_bounds_2d(scene, cam_ob, me_ob):
+    """
+    Returns camera space bounding box of mesh object.
+
+    Negative 'z' value means the point is behind the camera.
+
+    Takes shift-x/y, lens angle and sensor size into account
+    as well as perspective/ortho projections.
+
+    :arg scene: Scene to use for frame size.
+    :type scene: :class:`bpy.types.Scene`
+    :arg obj: Camera object.
+    :type obj: :class:`bpy.types.Object`
+    :arg me: Untransformed Mesh.
+    :type me: :class:`bpy.types.Mesh´
+    :return: a Box object (call its to_tuple() method to get x, y, width and height)
+    :rtype: :class:`Box`
+    """
+
+    mat = cam_ob.matrix_world.normalized().inverted()
+    me = me_ob.to_mesh(scene, True, 'PREVIEW')
+    me.transform(me_ob.matrix_world)
+    me.transform(mat)
+
+    camera = cam_ob.data
+    frame = [-v for v in camera.view_frame(scene=scene)[:3]]
+    camera_persp = camera.type != 'ORTHO'
+
+    lx = []
+    ly = []
+
+    for v in me.vertices:
+        co_local = v.co
+        z = -co_local.z
+
+        if camera_persp:
+            if z == 0.0:
+                lx.append(0.5)
+                ly.append(0.5)
+            # Does it make any sense to drop these?
+            #if z <= 0.0:
+            #    continue
+            else:
+                frame = [(v / (v.z / z)) for v in frame]
+
+        min_x, max_x = frame[1].x, frame[2].x
+        min_y, max_y = frame[0].y, frame[1].y
+
+        x = (co_local.x - min_x) / (max_x - min_x)
+        y = (co_local.y - min_y) / (max_y - min_y)
+
+        lx.append(x)
+        ly.append(y)
+
+    min_x = clamp(min(lx), 0.0, 1.0)
+    max_x = clamp(max(lx), 0.0, 1.0)
+    min_y = clamp(min(ly), 0.0, 1.0)
+    max_y = clamp(max(ly), 0.0, 1.0)
+
+    bpy.data.meshes.remove(me)
+
+    r = scene.render
+    fac = r.resolution_percentage * 0.01
+    dim_x = r.resolution_x * fac
+    dim_y = r.resolution_y * fac
+
+    return Box(min_x, min_y, max_x, max_y, dim_x, dim_y)
+'''
+Produce AVI video for stimuli
+'''
+def produce_video(camera_position,type_bird, colition,final_frame,L, bird_position = (14.15445,23.74561,4.82148),FPS = 24.0):
 
 	import bpy
 	import pickle
-	from math import *
-	import numpy
+	import math
+	import random
 	import mathutils
-	from camera_view import camera_view_bounds_2d
 
 	def look_at(obj_camera, point):
 	    loc_camera = obj_camera.matrix_world.to_translation()
@@ -17,7 +158,7 @@ def produce_video(camera_position, bird_position = (14.15445,23.74561,4.82148),F
 	    obj_camera.keyframe_insert(data_path ='Rotation',group="LocRot")
 
 	def get_finalposition(camera,L):
-		vec = mathutils.Vector((L*sin(pi*numpy.random.uniform(1),0.0,L*cos(pi*numpy.random.uniform(1)))))
+		vec = mathutils.Vector((L*sin(pi*random.uniform(0,1),0.0,L*cos(pi*random.uniform(0,1)))))
 		inv = camera.matrix_world.copy()
 		inv.invert()
 		# vec aligned to local axis
@@ -51,27 +192,28 @@ def produce_video(camera_position, bird_position = (14.15445,23.74561,4.82148),F
 	scene.frame_end = final_frame
 	obj_camera = bpy.data.objects["Camera"]
 	for group in data_dst.groups:
-	    ob = bpy.data.objects.new(group.name, None)
-	    ob.dupli_group = group
-	    ob.dupli_type = 'GROUP'
-	    scene.objects.link(ob)
+		ob = bpy.data.objects.new(group.name, None)
+		ob.dupli_group = group
+		ob.dupli_type = 'GROUP'
+		scene.objects.link(ob)
 	    #look_at(ob, obj_camera.matrix_world.to_translation())
 
-	#obj_camera.select = True
-	#ob.select = True
-	    ob.select = True
-	    bpy.context.scene.frame_set(1)
-	    if type_bird == 1:
-	    	ob.scale = (0.4,0.4,0.4)
-	    ob.location = bird_position
-	    ob.keyframe_insert(data_path ='location',group="LocRot")
+		#obj_camera.select = True
+		#ob.select = True
+		ob.select = True
+		bpy.context.scene.frame_set(1)
+		if type_bird == 1:
+	    		ob.scale = (0.4,0.4,0.4)
+	    		ob.location = bird_position
+	    		ob.keyframe_insert(data_path ='location',group="LocRot")
 
-	    bpy.context.scene.frame_set(final_frame)
-	    if colition == 1:
-	    	ob.location = tuple(obj_camera.location)
-	    else:
+		bpy.context.scene.frame_set(final_frame)
+		if colition == 1:
+	    		ob.location = tuple(obj_camera.location)
+		else:
 			ob.location = get_finalposition(obj_camera,L)
-	    ob.keyframe_insert(data_path ='location',group='LocRot')
+
+		ob.keyframe_insert(data_path ='location',group='LocRot')
 
 
 
@@ -87,10 +229,10 @@ def produce_video(camera_position, bird_position = (14.15445,23.74561,4.82148),F
 		# calulate the distance of the two objects
 		tuple_position = camera_view_bounds_2d(scene, obj_camera,ob)
 		print(tuple_position)
-		distance = sqrt((lst[0][0] - lst[1][0])**2 + (lst[0][1] - lst[1][1])**2 + (lst[0][2] - lst[1][2])**2)
+		distance = math.sqrt((lst[0][0] - lst[1][0])**2 + (lst[0][1] - lst[1][1])**2 + (lst[0][2] - lst[1][2])**2)
 		distances.append(distance)
 
-'''
+
 	#Save distance to the camera for further analysis
 	f = open('data.pkl','wb')
 	pickle.dump(distances,f,2)
@@ -103,6 +245,5 @@ def produce_video(camera_position, bird_position = (14.15445,23.74561,4.82148),F
 	bpy.ops.render.render( animation=True )
 
 
-'''
 
 produce_video(camera_position = (4.15445,13.74561,-4.82148), bird_position = (14.15445,23.74561,4.82148),FPS = 24.0,type_bird=1, colition=1,final_frame=115,L=2)
